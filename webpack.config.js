fa5d6e321f0a94d509e612478d813e8b661eac0b
Aowei Chenqi <aoweichenqi@Aoweis-MacBook-Pro.local>
2020-11-11 20:15:06 -0800

    Setup React

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/src');
+var DIST_DIR = path.join(__dirname,'/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env' ]
+        }
+      }
+    ]
+  },
+  watch: true
+};
