74327d1ee338762fddb86408ab039c757508bb47
Aowei Chenqi <aoweichenqi@Aoweis-MacBook-Pro.local>
2021-01-04 21:05:08 -0800

    Authentication Fixes

--- server/mongodb.js
+++ server/mongodb.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 const key = require('./mongokey.js');
 //ongoose.connect('mongodb://54.176.228.90:27017/reviews', { useNewUrlParser: true });
-
-mongoose.connect(`mongodb://${key.user}:${key.pass}@${key.ip}/reviews`, { useNewUrlParser: true });
+console.log(key);
+mongoose.connect(`mongodb://${key.ip}/reviews`, {
+  useNewUrlParser: true,
+  auth: {
+    user: key.user,
+    password:key.pass
+  },
+  authSource:"admin"
+ });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
